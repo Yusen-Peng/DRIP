9379145a365e5e941b2f32b87714eaa061412fa6
Yusen-Peng <peng.1007@buckeyemail.osu.edu>
2025-10-18 14:03:18 -0400

    POPE benchmark evaluation

---
+++ scripts/task3/eval_pope.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=POPE_DRIP
+#SBATCH --output=POPE_DRIP.txt
+#SBATCH --time=00:20:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128G
+#SBATCH --account=PAS2836
+
+module load miniconda3/24.1.2-py310
+conda activate DRIP
+source activate DRIP
+
+export OMP_NUM_THREADS=16
+export MASTER_PORT=$((12000 + RANDOM % 20000))
+
+cd /users/PAS2912/yusenpeng/Fast-CLIP/
+
+
+##### /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/pope
+
+mkdir -p /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/pope/answers
+touch /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/pope/answers/DRIP.jsonl
+
+#### /fs/scratch/PAS2836/yusenpeng_checkpoint/ViT-base-finetune
+
+python src/model_vqa_loader.py \
+    --model-path /fs/scratch/PAS2836/yusenpeng_checkpoint/XLbased-DRIP-4x-16-2-10-finetune \
+    --model-base lmsys/vicuna-7b-v1.5 \
+    --question-file /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/pope/llava_pope_test.jsonl \
+    --image-folder /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/pope/ \
+    --answers-file /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/pope/answers/DRIP.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+python src/eval_pope.py \
+    --annotation-dir /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/pope/anno \
+    --question-file /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/pope/llava_pope_test.jsonl \
+    --result-file /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/pope/answers/DRIP.jsonl
