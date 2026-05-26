0bf27a97274196c5fb575dbd81a7738a1f4fb541
Yusen-Peng <peng.1007@buckeyemail.osu.edu>
2026-05-26 08:34:21 -0400

    updated OCRBench script

--- scripts/task3/eval/eval_ocrbench.sh
+++ scripts/task3/eval/eval_ocrbench.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=May25_OCRBench_lora
-#SBATCH --output=May25_OCRBench_lora.log
+#SBATCH --job-name=May25_OCRBench_LLaVA_7B_PruMerge_8x_pretrain
+#SBATCH --output=May25_OCRBench_LLaVA_7B_PruMerge_8x_pretrain.log
 #SBATCH --time=00:20:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -19,22 +19,11 @@ export MASTER_PORT=$((12000 + RANDOM % 20000))
 
 cd /users/PAS2912/yusenpeng/DRIP/
 
-VERSION="LLaVA_7B_checkpoint_lora"
-
-# python src/model_vqa_ocrbench.py \
-#     --model_path /fs/scratch/PAS2836/yusenpeng_checkpoint/llava-v1.5-7b-local \
-#     --image_folder /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/ocrbench/OCRBench_Images \
-#     --output_folder /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/ocrbench/results \
-#     --save_name ${VERSION} \
-#     --OCRBench_file /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/ocrbench/OCRBench.json \
-#     --temperature 0 \
-#     --conv_mode vicuna_v1 \
-#     --num_workers 1
+VERSION="LLaVA_7B_PruMerge_8x_pretrain"
 
 
 python src/model_vqa_ocrbench.py \
-    --model_path /fs/scratch/PAS2836/yusenpeng_checkpoint/llava-v1.5-7b-lora-local \
-    --model_base lmsys/vicuna-7b-v1.5 \
+    --model_path /fs/scratch/PAS2836/yusenpeng_checkpoint/llava-v1.5-7b-local \
     --image_folder /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/ocrbench/OCRBench_Images \
     --output_folder /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/ocrbench/results \
     --save_name ${VERSION} \
@@ -42,5 +31,16 @@ python src/model_vqa_ocrbench.py \
     --temperature 0 \
     --conv_mode vicuna_v1 \
     --num_workers 1
+
+# python src/model_vqa_ocrbench.py \
+#     --model_path /fs/scratch/PAS2836/yusenpeng_checkpoint/llava-v1.5-7b-lora-local \
+#     --model_base lmsys/vicuna-7b-v1.5 \
+#     --image_folder /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/ocrbench/OCRBench_Images \
+#     --output_folder /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/ocrbench/results \
+#     --save_name ${VERSION} \
+#     --OCRBench_file /fs/scratch/PAS2836/yusenpeng_dataset/LLaVA_eval/ocrbench/OCRBench.json \
+#     --temperature 0 \
+#     --conv_mode vicuna_v1 \
+#     --num_workers 1
 
 conda deactivate
