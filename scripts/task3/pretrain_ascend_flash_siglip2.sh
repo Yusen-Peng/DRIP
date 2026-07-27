ea2a9995459b00ffe9f4254286bfc7e0a49800cb
Yusen-Peng <peng.1007@buckeyemail.osu.edu>
2026-07-26 21:15:08 -0400

    a grid search of temperature for SigLIP 2

--- scripts/task3/pretrain_ascend_flash_siglip2.sh
+++ scripts/task3/pretrain_ascend_flash_siglip2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=July4_LLaVA_7B_SigLIP_HF_v2_DRIP_10x_pretrain
-#SBATCH --output=July4_LLaVA_7B_SigLIP_HF_v2_DRIP_10x_pretrain.txt
+#SBATCH --job-name=July25_LLaVA_7B_SigLIP_HF_v2_DRIP_10x_pretrain_temp08
+#SBATCH --output=July25_LLaVA_7B_SigLIP_HF_v2_DRIP_10x_pretrain_temp08.txt
 #SBATCH --time=15:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -35,7 +35,7 @@ deepspeed --num_gpus=1 src/task3_llava.py \
     --mm_vision_select_layer -1 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --output_dir /fs/scratch/PAS2836/yusenpeng_checkpoint/LLaVA_7B_SigLIP_HF_v2_DRIP_10x_pretrain_temp001 \
+    --output_dir /fs/scratch/PAS2836/yusenpeng_checkpoint/LLaVA_7B_SigLIP_HF_v2_DRIP_10x_pretrain_temp08 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
